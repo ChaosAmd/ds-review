70e3e8c17fb0b33e87d2c880dc4565bd7cf00239
Amadeus Dabela <amadeus.cs@icloud.com>
2020-03-15 10:05:38 -0300

    Change main output

--- vector/c/main.c
+++ vector/c/main.c
@@ -5,7 +5,7 @@ int main() {
     vector vec;
     vector_init(&vec);
 
-    printf("1. Check the initialization of vector: \n");
+    printf("1. The initialization of vector: \n");
     printf("\t1.1 Initial Size: %d.\n", vector_size(&vec));
     printf("\t1.2 Initial capacity: %d.\n", vector_capacity(&vec));
     printf("\t1.3 Is empty: %d\n", is_empty_vector(&vec));
